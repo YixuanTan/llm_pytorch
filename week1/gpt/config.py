3018530c3057c513b0426fb0e2d9c0025f1c2f84
ethan.tan <yixuan.ethan.tan@gmail.com>
2025-11-30 16:23:44 -0800

    update

---
+++ week1/gpt/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass 
+
+@dataclass
+class GPTConfig:
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    block_size: int = 1024
+    dropout: float = 0.0
+
